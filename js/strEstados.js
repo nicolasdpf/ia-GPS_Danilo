9b0669e41d276243e63653de1c5b219e0359a42f
danntastico <nicolasdpf@gmail.com>
2019-04-14 14:47:49 -0500

    asdas

--- js/strEstados.js
+++ js/strEstados.js
@@ -7,11 +7,11 @@ var Estados = [
     //0
     {ciudad : "Leticia",
     ubicacion : {lat:-3.6150525,long:-70.3169899},
-    vecinos : [{ciudad: 22}, {ciudad: 7}, {ciudad: 30}]},
+    vecinos : [{ciudad: 22, distancia: 955}, {ciudad: 7, distancia: 902}, {ciudad: 30, distancia: 607}]},
     //1
     {ciudad : "Medellin",
     ubicacion : {lat: 6.2518401, long: -75.563591},
-    vecinos : [{ciudad:12}, {ciudad : 4}, { ciudad: 26}, {ciudad: 5}, { ciudad : 6}, {ciudad: 24},
+    vecinos : [{ciudad:12, distancia: 403}, {ciudad : 4, distancia: 642}, { ciudad: 26, distancia: 392}, {ciudad: 5}, { ciudad : 6}, {ciudad: 24},
         {ciudad: 11}]},
     //2
     {ciudad : "Arauca",
@@ -37,7 +37,7 @@ var Estados = [
     {ciudad : "Florencia",
     ubicacion : { lat: 1.61438,	long: -75.6062317},
     vecinos : [{ciudad: 22},{ciudad: 9}, {ciudad: 16}, {ciudad: 19},
-        {ciudad: 15}, {ciudad: 30}, {ciudad: 0}]},
+        {ciudad: 15}, {ciudad: 30}, {ciudad: 0, distancia: 902}]},
 //8
     {ciudad : "Yopal",
     ubicacion : { lat: 5.33775, long: -72.3958588},
@@ -95,19 +95,21 @@ var Estados = [
 //21
     {ciudad : "Cucuta",
     ubicacion : { lat: 7.8939099, long: -72.5078201},
-    vecinos : []},
+    vecinos : [{ciudad: 10}, {ciudad: 26} , {ciudad: 5}, 
+        {ciudad: 2}]},
 //22
     {ciudad : "Mocoa",
     ubicacion : { lat: 1.14748 , long: -76.6473}, /** */
-    vecinos : []},
+    vecinos : [{ciudad: 20}, {ciudad: 9}, {ciudad: 7},
+        {ciudad: 0, distancia: 955}]},
 //23
     {ciudad : "Armenia",
     ubicacion : { lat: 4.5338898, long: -75.6811066},
-    vecinos : []},
+    vecinos : [{ciudad: 24}, {ciudad: 29}, {ciudad: 28}]},
 //24
     {ciudad : "Pereira",
     ubicacion : { lat: 4.8133302, long: -75.6961136},
-    vecinos : []},
+    vecinos : [{ciudad: 23},{ciudad: 29}, {ciudad: 11}, {ciudad: 6}, {ciudad: 28}]},
 //25
     {ciudad : "San Andres",
     ubicacion : { lat: 12.5847197, long: -81.7005615},
@@ -115,25 +117,25 @@ var Estados = [
 //26
     {ciudad : "Bucaramanga",
     ubicacion : { lat: 7.1253901, long: -73.1197968},
-    vecinos : []},
+    vecinos : [{ciudad: 21}, {ciudad: 10}, {ciudad: 4}, {ciudad: 1}, {ciudad: 5}]},
 //27
     {ciudad : "Sincelejo",
     ubicacion : { lat: 9.3047199, long:	-75.3977814},
-    vecinos : []},
+    vecinos : [{ciudad: 12}, {ciudad: 4}]},
 //28
     {ciudad : "Ibague",
     ubicacion : { lat: 4.43889, long: -75.2322235},
-    vecinos : [{ciudad: 28}, {}, {}]},
+    vecinos : [{ciudad: 23}, {ciudad: 24}, {ciudad: 6}, {ciudad: 13}, {ciudad: 16}, {ciudad: 29} ]},
 //29
     {ciudad : "Cali",
     ubicacion : { lat: 3.4372201, long: -76.5224991},
-    vecinos : []},
+    vecinos : [{ciudad: 11}, {ciudad: 24}, {ciudad: 1}, {ciudad: 28}, {ciudad: 9}]},
 //30
     {ciudad : "Mitu",
     ubicacion : { lat: 1.25509, long: -70.235}, /** */
-    vecinos : []},
+    vecinos : [{ciudad: 0}, {ciudad: 7}, {ciudad: 15}, {ciudad: 14}]},
 //31
     {ciudad: "Puerto Carreño",
     ubicacion:  {lat: 6.1890300, long: -67.4858800},
-    vecinos: []}
+    vecinos: [{ciudad: 14}, {ciudad: 15}, {ciudad: 19}, {ciudad: 8}, {ciudad: 2}]}
 ];
