bc470fb8dbce272faf9cf408f8438f5f7c9891ab
danntastico <nicolasdpf@gmail.com>
2019-04-15 19:49:11 -0500

    new Cola

--- js/Cola.js
+++ js/Cola.js
@@ -1,3 +1,24 @@
 class Cola{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+    dequeue(val){
+        if(!this.first) return null;
+        var tem
+    }
 
 }
