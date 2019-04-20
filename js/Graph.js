50a4bb4f515fea525bfafde50d009d5d6bbec6bb
danntastico <nicolasdpf@gmail.com>
2019-04-19 22:24:05 -0500

    MyChanges

--- js/Graph.js
+++ js/Graph.js
@@ -22,4 +22,44 @@ class Graph{
             v => v !== vertex1
         )
     }
+
+
+    depthFirstRecursive(start){
+        const result = []
+        const visited = {}
+        const dataList = this.dataList;
+
+        (function dfs(vertex){
+            if(!vertex) return null
+            visited[vertex] = true;
+            result.push(vertex)
+            dataList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+        return result
+    }
+    breadthFirst(start){
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+           
+
+            this.dataList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 }
