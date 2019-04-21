319c3545b1d70105119b2831e4e8dd673da4e137
Danntastico <32252648+nicolasdpf@users.noreply.github.com>
2019-04-21 15:33:13 -0500

    Dios no me sonrie pero  igual acabaré

--- js/GoogleMaps.js
+++ js/GoogleMaps.js
@@ -270,21 +270,39 @@ function initMap() {
     
 }
 
-function trazarRuta(pasos, costo, metodo) {
-    imprimirRuta(pasos, costo, metodo);
-    let puntos = new Array();
-    for (let i = 1; i < pasos.length - 1; i++) {
-        puntos.push({ location: Ciudades[pasos[i]].center, stopover: true });
-    }
-    let camino = {
-        destination: Ciudades[pasos[pasos.length - 1]].center,
-        origin: Ciudades[pasos[0]].center,
-        travelMode: 'DRIVING',
-        waypoints: puntos
-    };
-    dirServ.route(camino, function(response, status) {
-        if (status == 'OK') {
-            dirRender.setDirections(response);
-        }
-    });
+// function trazarRuta(pasos) {
+//     let puntos = new Array();
+//     for (let i = 1; i < pasos.length - 1; i++) {
+//         puntos.push({ location: Estados[pasos[i]].ubicacion, stopover: true });
+//     }
+//     let camino = {
+//         destination: Estados[pasos[pasos.length - 1]].ubicacion,
+//         origin: Estados[pasos[0]].ubicacion,
+//         travelMode: 'DRIVING',
+//         waypoints: puntos
+//     };
+//     dirServ.route(camino, function(response, status) {
+//         if (status == 'OK') {
+//             dirRender.setDirections(response);
+//         }
+//     });
+// }
+
+function displayRoute(result){
+    let point = []
+    console.log(Estados[result[0]]);
+    // for (let index = 0; index < result.length; index++) {
+    //     point.push({location: Estados[result.indexOf(result[i])].ubicacion, stopover: true});        
+    // }
+    // let camino = {
+    //     destination: Estados[result.indexOf(result[pasos.length - 1])].ubicacion,
+    //     origin: Estados[result.indexOf(result[0])].ubicacion,
+    //     travelMode: 'DRIVING',
+    //     waypoints: puntos
+    // };
+    // dirServ.route(camino, function(response, status) {
+    //     if (status == 'OK') {
+    //         dirRender.setDirections(response);
+    //     }
+    // });
 }
