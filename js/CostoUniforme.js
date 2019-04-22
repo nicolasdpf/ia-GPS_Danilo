eac51d32986a5abc2aa211893aae5b0cb30d1f60
danntastico <nicolasdpf@gmail.com>
2019-04-22 08:37:30 -0500

    Last

--- js/CostoUniforme.js
+++ js/CostoUniforme.js
@@ -1,3 +1,7 @@
+/**
+ * 
+ * 
+ * **/
 class PriorityQueue {
     constructor(){
       this.values = [];
@@ -28,54 +32,70 @@ class PriorityQueue {
           //Relaciona al vertice con otro vertice y asigna el costo de dicha arista
           this.adjacencyList[vertex1].push({node:vertex2,weight});
       }
-      Dijkstra(start, finish){
-          const nodes = new PriorityQueue();
-          const distances = {};
-          const previous = {};
-          let path = [] //to return at end
-          let smallest;
-          //build up initial state
-          for(let vertex in this.adjacencyList){
-              if(vertex === start){
+
+      /**
+       * 
+       * @param {Vertice origen} start 
+       * @param {Vertice destino} finish 
+       */
+      costoUniformeD(origen, destino){
+          const nodes = new PriorityQueue(); //Priority Queue: Cola que organiza elementos segun un indice de prioridad
+          const distances = {}; //Objeto donde se guardan las distancias
+          const previous = {}; //Objeto que guarda todos los vertices estudiados con anterioridad
+          let path = [] //Resultado
+          let nodoMenor; 
+
+          /**
+           * Primero se asignan todos los vertices al objeto distancias
+           * En un principio todos los nodos de la lista de adyacencia deben tener un valor infinito
+           * excepto el nodo inicial, cuyo valor es cero (unico valor conocido)
+           * 
+           */
+          for(let vertex in this.adjacencyList){ //Para cada valor de la lista de adyacencia
+              if(vertex === origen){ //Si el valor es igual al vertice de inicio, añadir a cola de prioridad y objeto distancias
+                //Y asignar valor cero
                   distances[vertex] = 0;
                   nodes.enqueue(vertex, 0);
               } else {
+                  //Para los demas, valor infinito
                   distances[vertex] = Infinity;
                   nodes.enqueue(vertex, Infinity);
               }
               previous[vertex] = null;
           }
-          // as long as there is something to visit
+          //Si en la cola de nodos hay datos, ejecuta 
           while(nodes.values.length){
-              smallest = nodes.dequeue().val;
-              if(smallest === finish){
-                  //WE ARE DONE
-                  //BUILD UP PATH TO RETURN AT END
-                  while(previous[smallest]){
-                      path.push(smallest);
-                      smallest = previous[smallest];
+              //guarda el nodo más pequeño, desencolado de la cola de prioridad Nodes
+              nodoMenor = nodes.dequeue().val;
+              if(nodoMenor === destino){
+                  //si el nodo más pequeño es el vertice destino
+                  while(previous[nodoMenor]){
+                      //Mientras exista el nodo en la lista previous, el nodo se añade a la pila path
+                      path.push(nodoMenor);
+                      nodoMenor = previous[nodoMenor];
                   }
                   break;
               } 
-              if(smallest || distances[smallest] !== Infinity){
-                  for(let neighbor in this.adjacencyList[smallest]){
-                      //find neighboring node
-                      let nextNode = this.adjacencyList[smallest][neighbor];
-                      //calculate new distance to neighboring node
-                      let candidate = distances[smallest] + nextNode.weight;
+              if(nodoMenor || distances[nodoMenor] !== Infinity){
+                  for(let neighbor in this.adjacencyList[nodoMenor]){
+                      //Busca todos los nodos vecinos en la lista de adyacencia del nodo de prioridad menor
+                      let nextNode = this.adjacencyList[nodoMenor][neighbor];
+                      //Calcula nueva distancia al Nodo vecino sumando la distancia actual+ coste del vecino
+                      let candidato = distances[nodoMenor] + nextNode.weight;
+
                       let nextNeighbor = nextNode.node;
-                      if(candidate < distances[nextNeighbor]){
-                          //updating new smallest distance to neighbor
-                          distances[nextNeighbor] = candidate;
-                          //updating previous - How we got to neighbor
-                          previous[nextNeighbor] = smallest;
-                          //enqueue in priority queue with new priority
-                          nodes.enqueue(nextNeighbor, candidate);
+                      if(candidato < distances[nextNeighbor]){
+                          
+                          distances[nextNeighbor] = candidato;
+                          
+                          previous[nextNeighbor] = nodoMenor;
+                          
+                          nodes.enqueue(nextNeighbor, candidato);
                       }
                   }
               }
           }
-          return path.concat(smallest).reverse();     
+          return path.concat(nodoMenor).reverse();     
       }
   }
   
